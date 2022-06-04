c4a97f2a2c711b744443ff9a7698d0d390705ff6
Quentin Lem <quentinlemcode@gmail.com>
2022-06-04 10:46:11 +0200

    replaced dotenv by configmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MusicController } from './music/music.controller';
 import { SpotifyApiService } from './external/spotify-api/spotify-api.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, ConfigModule.forRoot()],
   controllers: [AppController, MusicController],
   providers: [AppService, SpotifyApiService],
 })
