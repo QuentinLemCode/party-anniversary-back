7c85508366f4e8b2068315fd5a6e03f2b09b93ec
Quentin Lem <quentinlemcode@gmail.com>
2022-06-04 10:27:54 +0200

    Added spotify login endpoint

--- src/music/music.controller.ts
+++ src/music/music.controller.ts
@@ -1,8 +1,8 @@
 import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 import { SpotifyApiService } from '../external/spotify-api/spotify-api.service';
 import { SearchResults } from '../external/spotify-api/spotify-interfaces';
 import { Music } from '../interfaces/music';
-
 @Controller('music')
 export class MusicController {
   constructor(private spotify: SpotifyApiService) {}
@@ -17,6 +17,27 @@ export class MusicController {
         },
       });
     });
+  }
+
+  @Get('spotify-login')
+  spotifyLogin() {
+    const state = randomUUID();
+    const scope =
+      'user-modify-playback-state user-read-playback-state user-read-currently-playing user-read-recently-played user-read-playback-state';
+
+    const url = new URL('https://accouts.spotify.com/authorize');
+
+    const params = {
+      response_type: 'code',
+      client_id: process.env.SPOTIFY_CLIENT_ID,
+      scope: scope,
+      redirect_uri: 'http://localhost:4200/admin/spotify_auth',
+      state: state,
+    };
+    Object.entries(params).forEach(([key, value]) => {
+      url.searchParams.set(key, value);
+    });
+    return url.toString();
   }
 
   private mapResults(results: SearchResults): Music[] {
