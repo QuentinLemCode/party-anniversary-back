6018c43d7af6c4b1542c069bf256dc33ba8e7383
Quentin Lem <quentinlemcode@gmail.com>
2022-06-05 11:33:30 +0200

    removed app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MusicController } from './music/music.controller';
-import { SpotifyApiService } from './external/spotify-api/spotify-api.service';
 import { ConfigModule } from '@nestjs/config';
+import { SpotifyApiService } from './external/spotify-api/spotify-api.service';
+import { MusicController } from './music/music.controller';
 
 @Module({
   imports: [HttpModule, ConfigModule.forRoot()],
-  controllers: [AppController, MusicController],
-  providers: [AppService, SpotifyApiService],
+  controllers: [MusicController],
+  providers: [SpotifyApiService],
 })
 export class AppModule {}
