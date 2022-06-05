8973d0845a69146327f384f8af9aae125d6f55cd
Quentin Lem <quentinlemcode@gmail.com>
2022-06-05 11:41:43 +0200

    Replaced sqlite by better-sqlite3 (let's give a try)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,17 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { SpotifyApiService } from './external/spotify-api/spotify-api.service';
 import { MusicController } from './music/music.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [HttpModule, ConfigModule.forRoot()],
+  imports: [
+    HttpModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: './database/db.sql',
+    }),
+  ],
   controllers: [MusicController],
   providers: [SpotifyApiService],
 })
