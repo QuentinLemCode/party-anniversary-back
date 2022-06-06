0a5b93105902a3ba46f395b7b1b6dade18a6b30b
Quentin Lem <quentinlemcode@gmail.com>
2022-06-06 08:16:55 +0200

    fix tests

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './user.entity';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
@@ -6,7 +8,15 @@ describe('UserService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {
+            findOne: () => null,
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
